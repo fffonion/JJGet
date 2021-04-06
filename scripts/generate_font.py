720822099a5209a38e6535e1abe9b060e1d998e5
Wangchong Zhou <fffonion@gmail.com>
2021-04-06 18:27:11 +0800

    无监督自学习

--- scripts/generate_font.py
+++ scripts/generate_font.py
@@ -21,94 +21,161 @@ if not os.path.exists("fonts"):
     os.makedirs("fonts")
 
 PER_LINE = 20
+CHAR_LT = 10000
+FUZZ = 20
+CONFIDENCY = 0.4
+HIGH_CONFIDENCY = 0.9
 
-# id: char
-known_chars = {}
-# id: coord
+PLACEHOLDER_LEARNED = "-------"
+
+# LEARN
+# char: coord
 coord_map = {}
+coord_map_candidate = {}
+candidate_names = set()
 
-def load_existing_result():
-    global known_chars
+def is_glpyh_similar(a, b, fuzz):
+    if len(a) != len(b):
+        return False
+    found = True
+    for i in range(len(a)):
+        if abs(a[i][0] - b[i][0]) > fuzz or abs(a[i][1] - b[i][1]) > fuzz:
+            found = False
+            break
+    return found
+
+def load_candidate(name):
+    global coord_map_candidate
+    global candidate_names
+
+    with open(os.path.join("results-ocr", name + ".json")) as f:
+        try:
+            m = json.loads(f.read())             
+        except:
+            return
+    ttf_path = os.path.join("fonts", name + ".ttf")
+    if not os.path.exists(ttf_path):
+        return
+    if name in candidate_names:
+        print("skip already loaded %s" % name)
+        return
+
+    with TTFont(ttf_path, 0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1) as ttf:
+        cmap = ttf["cmap"]
+        known = set()
+        for x in cmap.tables:
+            for y in x.cmap.items():
+                if y[0] < CHAR_LT:
+                    continue
+                u = "%x" % y[0]
+                # OCR previously failed
+                if u not in m:
+                    continue
+                coord = ttf['glyf'][y[1]].coordinates
+                # one coordinates only adds once in each font
+                coord_key = str(coord)
+                if coord_key in known:
+                    continue
+                known.add(coord_key)
+
+                i = m[u]
+                l = len(coord)
+                if i not in coord_map_candidate:
+                    coord_map_candidate[i] = {}
+                if l not in coord_map_candidate[i]:
+                    coord_map_candidate[i][l] = []
+                # the character is already learned
+                if coord_map_candidate[i][l] == PLACEHOLDER_LEARNED:
+                    continue
+                coord_map_candidate[i][l].append(list(coord))
+
+    candidate_names.add(name)
+    print(name + " loaded into candidates")
+
+def learn_candidate(fuzz):
     global coord_map
+
+    for k in coord_map_candidate:
+        cans_font_types = coord_map_candidate[k]
+        # lujing changdu
+        for path_length in cans_font_types:
+            cans = cans_font_types[path_length]
+            # skip known characters
+            if cans == None:
+                continue
+            if len(cans) == 1 or len(cans) < len(candidate_names) * CONFIDENCY:
+                print("%s doesn't have enough data to learn (has %d, need %d)" % (
+                    k, len(cans), len(candidate_names) * CONFIDENCY
+                ))
+                continue
+            agreed = 0
+            for i in range(1, len(cans)):
+                if is_glpyh_similar(cans[0], cans[i], fuzz):
+                    agreed += 1
+            if agreed > (len(cans) - 1) * CONFIDENCY:
+                print("learned %s with %d/%d" % (k, agreed, len(cans)))
+                coord_map[k] = cans[0]
+                if agreed > (len(cans) - 1) * HIGH_CONFIDENCY:
+                    # clear the candidate buffer, stop further learning
+                    cans_font_types[path_length] = PLACEHOLDER_LEARNED
+            else:
+                print(k, "failed", agreed, (len(cans) - 1) * CONFIDENCY)
+
+    print("total characters learned %d" % (len(coord_map)))
+
+    with open("fast_compare.json", "w") as f:
+        f.write(json.dumps([coord_map, coord_map_candidate, list(candidate_names)]))
+
+def preload(fuzz):
+    global coord_map
+    global coord_map_candidate
+    global candidate_names
 
     if os.path.exists("fast_compare.json"):
         with open("fast_compare.json") as f:
-           use_font, max_agreed, known_chars, coord_map = json.loads(f.read())
-        print("loaded %s as trusted charset (agreed by %s)" % (use_font, max_agreed))
-        return
+           [coord_map, coord_map_candidate, candidate_names] = json.loads(f.read())
+        print("loaded %d characters" % (len(coord_map)))
+        candidate_names = set(candidate_names)
 
-    result_chars = {}
     for p in os.listdir("results-ocr"):
-        if p.endswith(".json"):
-            with open(os.path.join("results-ocr", p)) as f:
-                try:
-                    c = json.loads(f.read())
-                    t = os.path.splitext(p)[0]
-                    result_chars[t] = c                    
-                except:
-                    pass
+        if not p.endswith(".json"):
+            continue
+        load_candidate(os.path.splitext(p)[0])
     
-    elected = {}
-    for c in result_chars:
-        ll = "".join(sorted(result_chars[c].values()))
-        if ll in elected:
-            elected[ll].append(c)
-        else:
-            elected[ll] = [c,]
-    
-    max_agreed = []
-    for c in elected:
-        agreed = elected[c]
-        if len(agreed) > len(max_agreed):
-            max_agreed = agreed
-    
-    if len(max_agreed) == 1:
-        print("not able to find a trusted charset")
-        return
-    
-    use_font = max_agreed[0]
-    print("use %s as trusted charset (agreed by %s)" % (use_font, max_agreed))
-    known_chars = result_chars[use_font]
-    
-    ttf = TTFont(os.path.join("fonts", use_font+".ttf"), 0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
-    cmap = ttf["cmap"]
-    for x in cmap.tables:
-        for y in x.cmap.items():
-            if y[0] < 10000:
-                continue
-            coord = ttf['glyf'][y[1]].coordinates
-            i = "%x" % y[0]
-            if i in coord_map:
-                continue
-            coord_map[i] = [x for x in coord]
-    
-    with open("fast_compare.json", "w") as f:
-        f.write(json.dumps([use_font, max_agreed, known_chars, coord_map]))
+    print("preload %d candidates" % len(candidate_names))
 
-def find_by_coord(ttf, fuzz=40):
+    learn_candidate(fuzz)
+
+# FIND
+def find_by_coord(ttf, fuzz):
     result = {}
     if not coord_map:
         return result
 
+    missing = set()
     cmap = ttf["cmap"]
     for x in cmap.tables:
         for y in x.cmap.items():
             tc = "%x" % y[0]
-            if tc in result:
+            if tc in result or y[0] < CHAR_LT:
                 continue
-            coord = [x for x in ttf['glyf'][y[1]].coordinates]
+            coord = list(ttf['glyf'][y[1]].coordinates)
             for rc in coord_map:
-                if rc in result or len(coord) != len(coord_map[rc]):
+                if len(coord) != len(coord_map[rc]):
                     continue
                 ico = coord_map[rc]
-                found = True
-                for i in range(len(coord)):
-                    if abs(coord[i][0] - ico[i][0]) > fuzz or abs(coord[i][1] - ico[i][1]) > fuzz:
-                        found = False
-                        break
-                if found:
+                if is_glpyh_similar(coord, ico, fuzz):
                     #print("found ", tc, "==", rc, "(", known_chars[rc], ")")
-                    result[tc] = known_chars[rc]
+                    result[tc] = rc
+                    if tc in missing:
+                        missing.remove(tc)
+                    break
+                # current char might match a different path length
+                # remember it and remove when found
+                if tc not in result:
+                    missing.add(tc)
+    for k in missing:
+        result[k] = "?"
     return result
 
 def find_by_ocr(ttf, font_name, temp, pointsize):
@@ -151,7 +218,7 @@ def find_by_ocr(ttf, font_name, temp, pointsize):
     
     return char_map
 
-def parse(font_name, temp, coord_fuzz=0):
+def parse(font_name, temp, coord_fuzz=40):
     result_path = os.path.join("results", "%s.json" % font_name)
     if os.path.exists(result_path):
         with open(result_path, "rb") as f:
@@ -171,11 +238,17 @@ def parse(font_name, temp, coord_fuzz=0):
     char_map = {}
     # try coord
     char_map = find_by_coord(ttf, coord_fuzz)
-    if not char_map or True:
+    missing = set()
+    for k in char_map:
+        if char_map[k] == "?":
+            missing.add(k)
+    if not char_map or len(missing):
+        if missing:
+            print("fast compare missing %d: %s" % (len(missing), ",".join(missing)))
         errors = []
         for sz in (64, 96):
             try:
-                find_by_ocr(ttf, font_name, temp, 64)
+                char_map = find_by_ocr(ttf, font_name, temp, 64)
             except Exception as ex:
                 errors.append("%d:%s" % (sz, str(ex)))
             else:
@@ -186,6 +259,9 @@ def parse(font_name, temp, coord_fuzz=0):
         # save result for fast compare
         with open(os.path.join("results-ocr", "%s.json" % font_name), "wb") as f:
             f.write(json.dumps(char_map).encode("utf-8"))
+        
+        load_candidate(font_name)
+        learn_candidate(coord_fuzz)
     else:
         print("solved by fast compare")
     
@@ -193,10 +269,6 @@ def parse(font_name, temp, coord_fuzz=0):
         f.write(json.dumps(char_map).encode("utf-8"))
     
     ttf.close()
-
-    # load fast matching maps
-    if not coord_map:
-        load_existing_result()
 
     return char_map
 
@@ -235,7 +307,7 @@ class RequestHandler(SimpleHTTPRequestHandler):
         self.wfile.write(json.dumps(ret).encode("utf-8"))
 
 if __name__ == '__main__':
-    load_existing_result()
+    preload(FUZZ)
 
     server = ThreadingServer(('localhost', 62228), RequestHandler)
     print('Starting server, use <Ctrl-C> to stop')
